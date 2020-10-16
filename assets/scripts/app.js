2519e8d279aa8285576fe95d514dc8bad2a135c9
mcidoni <matthewcidoni@gmail.com>
2020-10-16 16:53:43 -0400

    made more changes to app.js and css

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,8 @@
 'use strict'
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-// use require without a reference to ensure a file is bundled
-// require('./example')
+
 const events = require('./auth/events')
 $(() => {
-  // your JS code goes here
+
   // POST sign-up
   $('#sign-up-form').on('submit', events.onSignUp)
   // POST sign-in
@@ -14,5 +11,24 @@ $(() => {
   $('#change-password-form').on('submit', events.onChangePassword)
   // DELETE sign-out
   $('#sign-out').on('click', events.onSignOut)
-})
 
+  let currentPlayer = '✕'
+
+  // Our box click event handler
+  const onSquareClick = (event) => {
+    console.log('click')
+
+    // Select the box that was clicked, event.target
+    const box = $(event.target)
+
+    // Then set the text to the current player
+    box.text(currentPlayer)
+
+    // Change the current player
+    currentPlayer = currentPlayer === 'O' ? '✕' : 'O'
+  }
+
+  // Select all of the boxes, $('.box'), add an event listener so that `on`
+  // every 'click' the `onBoxClick` event handler is called.
+  $('.square').on('click', onSquareClick)
+})
